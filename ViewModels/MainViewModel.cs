c949783014d10eaef88223027b7c8f78d0712e5b
Abrham Sayd Martinez Corrales <abrham.said@gmail.com>
2022-10-30 16:49:48 -0700

    Commando agregado para agregar hijos al view

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using Microsoft.VisualBasic.ApplicationServices;
 using PrestamosWPF.Models;
 using PrestamosWPF.Repositories;
+using PrestamosWPF.Views;
 
 namespace PrestamosWPF.ViewModels
 {
@@ -135,11 +136,14 @@ namespace PrestamosWPF.ViewModels
             }
         }
 
+        public UserModel User { get; private set; }
+
        
 
         public ICommand AddCommand { get; }
         public ICommand updateCommand { get; }
         public ICommand GetUsersByUsernameCommand { get; }
+        public ICommand AddPrestamosCommand { get; }
 
         public MainViewModel()
         {
@@ -149,6 +153,13 @@ namespace PrestamosWPF.ViewModels
             CurrentUserAccount = new UserAccountModel();
             GetUsersByUsernameCommand = new ViewModelCommand(ExecuteGetUsersByUsername);
             LoadCurrentUserdata();
+            AddPrestamosCommand = new ViewModelCommand(ExecuteAddPrestamosCommand);
+        }
+
+        private void ExecuteAddPrestamosCommand(object obj)
+        {
+            var vistaPrestamos = new PrestamosView();
+
         }
 
         private void ExecuteGetUsersByUsername(object obj)
