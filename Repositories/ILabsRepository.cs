43db5f1d638fe5e838795c70354669f5ca2340da
Abrham Sayd Martinez Corrales <abrham.said@gmail.com>
2022-10-30 22:46:48 -0700

    Actualizaciones

---
+++ Repositories/ILabsRepository.cs
@@ -0,0 +1,18 @@
+﻿using PrestamosWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestamosWPF.Repositories
+{
+    public interface ILabsRepository
+    {
+        void Add(LabsModel labsModel);
+        void Edit(LabsModel labsModel);
+        void Remove(int id);
+        DataTable GetByAll();
+    }
+}
